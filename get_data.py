26195f5a3e2d20f0676151a843a8803d09f851ac
Jamie <ScruffyTheMoose@users.noreply.github.com>
2022-01-01 21:26:19 -0600

    collect etf data

--- get_data.py
+++ get_data.py
@@ -2,8 +2,22 @@ import yahoo_fin.stock_info as si
 import pandas as pd
 import datetime as dt
 
-start = dt.datetime(2015, 1, 1)
+# chosen start/end dates
+# different companies will have different ranges of available data so this will need to be handled during data processing
+start = dt.datetime(2010, 1, 1)
 end = dt.datetime(2021, 12, 1)
 
-output = si.get_data(ticker='VOO', start_date=start, end_date=end)
-output.to_csv(path_or_buf='testcsv.csv')
+# format for retrieving historical prices in DF format from yahoo and saving to csv for ease-of-use
+# output = si.get_data(ticker='VOO', start_date=start, end_date=end)
+# output.to_csv(path_or_buf='testcsv.csv')
+
+# ETFs that we'll be collecting data for
+etfs = pd.read_csv('etfs.csv')['etfs'].tolist()
+
+for symbol in etfs:
+    try:
+        output = si.get_data(ticker=symbol, start_date=start, end_date=end)
+        output.to_csv(path_or_buf=f"etf_data/{symbol}.csv")
+    except Exception:
+        print(symbol + "is bad, it needs to be killed with fire.")
+        etfs.remove(symbol)
