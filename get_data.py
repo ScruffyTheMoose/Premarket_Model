c2a5195d545a385c71bf06fe74d9919f443c7a21
Jamie <ScruffyTheMoose@users.noreply.github.com>
2022-01-01 21:28:07 -0600

    comment out block for saving data

--- get_data.py
+++ get_data.py
@@ -14,10 +14,12 @@ end = dt.datetime(2021, 12, 1)
 # ETFs that we'll be collecting data for
 etfs = pd.read_csv('etfs.csv')['etfs'].tolist()
 
-for symbol in etfs:
-    try:
-        output = si.get_data(ticker=symbol, start_date=start, end_date=end)
-        output.to_csv(path_or_buf=f"etf_data/{symbol}.csv")
-    except Exception:
-        print(symbol + "is bad, it needs to be killed with fire.")
-        etfs.remove(symbol)
+
+# collecting historical data from 2010-01-01 to 2021-12-01 and saving to csv
+# for symbol in etfs:
+#     try:
+#         output = si.get_data(ticker=symbol, start_date=start, end_date=end)
+#         output.to_csv(path_or_buf=f"etf_data/{symbol}.csv")
+#     except Exception:
+#         print(symbol + "is bad, it needs to be killed with fire.")
+#         etfs.remove(symbol)
